54c7a96238ef8338d1baaaa2d225f03b74e30edf
khaledgabr77 <khaledgabr77@gmail.com>
2024-05-01 15:49:43 +0300

    update tag_poses

--- localization_system_april/static_tf.py
+++ localization_system_april/static_tf.py
@@ -15,26 +15,20 @@ class PoseBroadcaster(Node):
         self.child_frame_id_ = self.get_parameter("child_frame_id").value
 
         param_path = os.path.join(
-            os.getcwd(),  # Get current working directory
+            os.getcwd(),  
             'install', 'localization_system_april', 'share', 'localization_system_april', 'tag_pose.yaml'
         )
 
-        # self.get_logger().info("Parameter file path: %s", param_path)
-
-
         with open(param_path, 'r') as f:
             params = yaml.safe_load(f)
-        
-        print(params)
-        tag_info = params.get('tag_pose', {})
-        
-        # # self.get_logger().info("HI")
-        # for tag_id, pose_value in tag_info.items():
-        #     pose_info = [float(value) for value in pose_value]
-        #     # self.get_logger().info(f'Tag Pose {tag_id}: {pose_info}')
-        #     print(f'Tag Pose {tag_id}: {pose_info}')
-        # # self.get_logger().info("BYE")
 
+        self.print_poses(params['tag_pose'])
+    
+    def print_poses(self, data_poses):
+        for key, value in data_poses.items():
+            if isinstance(value, dict):
+                for sub_key, sub_value in value.items():
+                    self.get_logger().info(f"{sub_key}: {sub_value}")
 
 def main(args=None):
     rclpy.init(args=args)
@@ -42,7 +36,6 @@ def main(args=None):
     rclpy.spin(pose_broadcaster)
     pose_broadcaster.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
